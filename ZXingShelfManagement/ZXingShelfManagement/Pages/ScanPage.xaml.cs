5e5401f995c39115d12cd69b61c0e07f8b371e9c
ちび <chibivaru@live.jp>
2020-07-10 17:32:59 +0900

    AutoFocus

--- ZXingShelfManagement/ZXingShelfManagement/Pages/ScanPage.xaml.cs
+++ ZXingShelfManagement/ZXingShelfManagement/Pages/ScanPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,6 +14,9 @@ namespace ZXingShelfManagement
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ScanPage : ContentPage, ITaskListener
     {
+        private BackgroundWorker autoFocus;
+        private bool autoFocusing;
+
         public ScanPage()
         {
             InitializeComponent();
@@ -20,10 +25,30 @@ namespace ZXingShelfManagement
             TaskHttpGet.Instance.Listener = this;
             this.Title = "バーコードスキャン";
             NavigationPage.SetBackButtonTitle(this, String.Empty);
+
+            autoFocusing = true;
+
+            autoFocus = new BackgroundWorker();
+            autoFocus.DoWork += AutoFocus_DoWork;
+            autoFocus.RunWorkerCompleted += AutoFocus_RunWorkerCompleted;
+        }
+
+
+        private void AutoFocus_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(250);
+            zxing?.AutoFocus();
+        }
+
+        private void AutoFocus_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (autoFocusing == true)
+                autoFocus.RunWorkerAsync();
         }
 
         public void Handle_OnScanResult(ZXing.Result result)
         {
+            autoFocusing = false;
             this.IsBusy = true;
             zxing.IsAnalyzing = false;
             TaskHttpGet.Instance.Run(result.Text);
@@ -31,8 +56,10 @@ namespace ZXingShelfManagement
 
         public void OnFailure()
         {
+            autoFocusing = true;
             this.IsBusy = false;
             zxing.IsAnalyzing = true;
+            autoFocus.RunWorkerAsync();
         }
 
         public void OnSuccess()
@@ -48,9 +75,17 @@ namespace ZXingShelfManagement
         {
             base.OnAppearing();
             zxing.IsScanning = true;
+            autoFocusing = true;
+            autoFocus.RunWorkerAsync();
 
             if (zxing.IsAnalyzing == false)
                 zxing.IsAnalyzing = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            autoFocusing = false;
+            base.OnDisappearing();
         }
     }
 }
